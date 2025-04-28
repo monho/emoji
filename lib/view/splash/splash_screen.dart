6da7705fd2e277bb59127eddfe713b410834463e
t2024-m0188 <t2024-m0188@t2024-m0188ui-MacBookAir.local>
2025-04-28 11:21:47 +0900

    Refactor: 디렉토리 수정

--- lib/view/splash/splash_screen.dart
+++ lib/view/splash/splash_screen.dart
@@ -1,4 +1,4 @@
-import 'package:emoji/terms/terms_screen.dart'; // 경로 주의!
+import 'package:emoji/view/terms/terms_screen.dart'; // 경로 주의!
 import 'package:emoji/view/chatroom/chatroom_view.dart';
 import 'package:flutter/material.dart';
 
